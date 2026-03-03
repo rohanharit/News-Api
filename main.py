fed7a3cfdaeadaf68323d12856ce47c14cc15e6b
rohanharit <rohann1789@gmail.com>
2026-03-03 12:27:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+query=input("what type of news you want: ")
+api="Write YOUR NEWS API HERE"
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-12-14&sortBy=publishedAt&apiKey={api}"
+print(url)
+r=requests.get(url)
+data=r.json()
+articles=data["articles"]
+for index,article in enumerate(articles):
+    print(index+1,article["title"],article["url"],article["content"],sep="\n")
+    print("\n")
